6cb8a5ca714e88e39e3be5ac83617080438b998b
Santunis Oliveira <santunis.oliveira@gmail.com>
2025-02-28 12:29:38 -0300

    atualizando dados do usuário

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Link, useNavigate } from 'react-router-dom'
 import { Container } from '../../components/container'
@@ -10,6 +10,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 
 import { auth } from '../../services/firebaseConnection'
 import { createUserWithEmailAndPassword, updateProfile, signOut } from 'firebase/auth'
+import { AuthContext } from '../../contexts/AuthContext'
 
 const schema = z.object({
   name: z.string().nonempty("O campo nome é obrigatório"),
@@ -20,6 +21,7 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>
 
 export function Register() {
+  const { handleInfoUser } = useContext(AuthContext)
   const navigate = useNavigate();
 
   const { register, handleSubmit, formState: { errors} } = useForm<FormData>({
@@ -42,6 +44,11 @@ export function Register() {
         displayName: data.name
       })
 
+      handleInfoUser({
+        uid: user.user.uid,
+        name: data.name,
+        email: data.email
+      })
       console.log("CADASTRADO COM SUCESSO!")
       navigate("/dashboard", { replace: true })
     })
