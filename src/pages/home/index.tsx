f2e7e9add363075333ed8270244cc0e7ac504925
Santunis Oliveira <santunis.oliveira@gmail.com>
2025-03-06 16:23:21 -0300

    buscando carros na página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,49 +1,113 @@
+import { useState, useEffect } from 'react'
 import { Container } from "../../components/container";
+import { Link } from 'react-router-dom'
+import { collection, query, getDocs, orderBy } from 'firebase/firestore'
+import { db } from '../../services/firebaseConnection'
 
+interface CarsProps{
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps{
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export function Home() {
-    return (
-        <Container>
-          <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
-            <input
-              className="w-full border-2 rounded-lg h-9 px-3 outline-none"
-              placeholder="Digite o nome do carro..."
-            />
-            <button
-              className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
-            >
-              Buscar
-            </button>
-          </section>
 
-          <h1 className="font-bold text-center mt-6 text-2xl mb-4">
-            Carros novos e usados em todo o Brasil
-          </h1>
+  const [cars, setCars] = useState<CarsProps[]>([])
 
-          <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            <section className="w-full bg-white rounded-lg">
-              <img
-                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                src="https://www.webmotors.com.br/imagens/prod/347874/MITSUBISHI_PAJERO_SPORT_2.4_16V_MIVEC_TURBO_DIESEL_HPE_AWD_AUTOMATICO_34787411342917186.webp"
-                alt="Carro"
-              />
-              <p className="font-bold mt-1 mb-2 px-2">Nome do carro</p>
+  useEffect(() => {
 
-              <div className="flex flex-col px-2">
-                <span className="text-zinc-700 mb-6">Ano 2025 | 20.000 Km</span>
-                <strong className="text-black font-medium text-xl">R$ 120.000</strong>
-              </div>
+    function loadCars(){
+      const carsRef = collection(db, "cars")
+      const queryRef = query(carsRef, orderBy("created", "desc"))
 
-              <div className="w-full h-px bg-slate-200 my-2"></div>
+      getDocs(queryRef)
+      .then((snapshot) => {
+        let listcars = [] as CarsProps[];
 
-              <div className="px-2 pb-2">
-                <span className="text-zinc-700">
-                  Brasília - DF
-                </span>
-              </div>
-            </section>
-          </main>
-        </Container>
-    )
+        snapshot.forEach( doc => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid
+          })
+        })
+
+        console.log(listcars)
+
+        setCars(listcars);
+
+      })
+    }
+
+    loadCars();
+  }, [])
+
+  return (
+      <Container>
+        <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+          <input
+            className="w-full border-2 rounded-lg h-9 px-3 outline-none"
+            placeholder="Digite o nome do carro..."
+          />
+          <button
+            className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
+          >
+            Buscar
+          </button>
+        </section>
+
+        <h1 className="font-bold text-center mt-6 text-2xl mb-4">
+          Carros novos e usados em todo o Brasil
+        </h1>
+
+        <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          
+          {cars.map( car => (
+
+            <Link key={car.id} to={`/car/${car.id}`} >
+              <section className="w-full bg-white rounded-lg">
+                <img
+                  className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                  src={car.images[0].url}
+                  alt="Carro"
+                />
+                <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
+
+                <div className="flex flex-col px-2">
+                  <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} Km</span>
+                  <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+                </div>
+
+                <div className="w-full h-px bg-slate-200 my-2"></div>
+
+                <div className="px-2 pb-2">
+                  <span className="text-zinc-700">
+                      {car.city}
+                  </span>
+                </div>
+              </section>
+            </Link>
+
+          ))}
+
+        </main>
+      </Container>
+  )
 }
   
