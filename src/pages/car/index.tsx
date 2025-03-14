14c4edc9747ec15e7143d3da76e28dd0b83cd149
Santunis Oliveira <santunis.oliveira@gmail.com>
2025-03-14 10:19:08 -0300

    Criando slider

--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -6,6 +6,8 @@ import { useParams } from 'react-router-dom'
 import { getDoc, doc } from 'firebase/firestore'
 import { db } from '../../services/firebaseConnection'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 
 interface CarsProps{
   id: string;
@@ -33,6 +35,7 @@ export function CarDatail() {
 
   const { id } = useParams();
   const [car, setCar] = useState<CarsProps>();
+  const [sliderPerView, setSliderPerView] = useState<number>(2);
 
   useEffect(() => {
 
@@ -65,57 +68,90 @@ export function CarDatail() {
 
   }, [id])
 
+  useEffect(() => {
+
+    function handleResize() {
+      if(window.innerWidth < 720) {
+        setSliderPerView(1);
+      } else {
+        setSliderPerView(2);
+      }
+    }
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize)
+
+    return() => {
+      window.removeEventListener("resize", handleResize)
+    }
+
+  }, [])
 
 
-    return (
-      <Container>
-        <h1>SLIDER</h1>
 
-        { car && (
-          <main className="w-full bg-white rounded-lg p-6 my-4">
-            <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
-              <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
-              <h1 className="font-bold text-3xl text-black">R$ {car?.price}</h1>
+  return (
+    <Container>
+      <Swiper 
+        slidesPerView={sliderPerView}
+        pagination={ { clickable: true } }
+        navigation
+      >
+        {car?.images.map( image => (
+          <SwiperSlide key={image.name}>
+            <img 
+              src={image.url}
+              className="w-full h-96 object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      { car && (
+        <main className="w-full bg-white rounded-lg p-6 my-4">
+          <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
+            <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
+            <h1 className="font-bold text-3xl text-black">R$ {car?.price}</h1>
+          </div>
+
+          <p>{car.model}</p>
+
+          <div className="flex w-full gap-6 my-4">
+            <div className="flex flex-col gap-4">
+              <div>
+                <p>Cidade</p>
+                <strong>{car?.city}</strong>
+              </div>
+              <div>
+                <p>Ano</p>
+                <strong>{car?.year}</strong>
+              </div>
             </div>
 
-            <p>{car.model}</p>
-
-            <div className="flex w-full gap-6 my-4">
-              <div className="flex flex-col gap-4">
-                <div>
-                  <p>Cidade</p>
-                  <strong>{car?.city}</strong>
-                </div>
-                <div>
-                  <p>Ano</p>
-                  <strong>{car?.year}</strong>
-                </div>
-              </div>
-
-              <div className="flex flex-col gap-4">
-                <div>
-                  <p>KM</p>
-                  <strong>{car?.km}</strong>
-                </div>
+            <div className="flex flex-col gap-4">
+              <div>
+                <p>KM</p>
+                <strong>{car?.km}</strong>
               </div>
             </div>
+          </div>
 
-            <strong>Descrição:</strong>
-            <p className="mb-4">{car?.description}</p>
+          <strong>Descrição:</strong>
+          <p className="mb-4">{car?.description}</p>
 
-            <strong>Telefone / Whastapp</strong>
-            <p>{car?.whatsapp}</p>
+          <strong>Telefone / Whastapp</strong>
+          <p>{car?.whatsapp}</p>
 
-            <a
-              className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-xl font-medium"
-            >
-              Conversar com o vendedor
-              <FaWhatsapp size={26} color="#FFF" />
-            </a>
+          <a
+            className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-xl font-medium"
+          >
+            Conversar com o vendedor
+            <FaWhatsapp size={26} color="#FFF" />
+          </a>
 
 
-          </main>
-        )}
-      </Container>
-    )
+        </main>
+      )}
+    </Container>
+  )
 }
