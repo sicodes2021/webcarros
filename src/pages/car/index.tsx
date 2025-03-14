0fd1d1d82e989e0eb306d75290ea657b41f6d558
Santunis Oliveira <santunis.oliveira@gmail.com>
2025-03-14 09:53:31 -0300

    Pagina detalhes do carro

--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -1,9 +1,121 @@
+import { useEffect, useState } from 'react'
+import { Container } from '../../components/container'
+import { FaWhatsapp } from 'react-icons/fa'
+import { useParams } from 'react-router-dom'
 
+import { getDoc, doc } from 'firebase/firestore'
+import { db } from '../../services/firebaseConnection'
+
+
+interface CarsProps{
+  id: string;
+  name: string;
+  model: string;
+  city: string;
+  year: string;
+  km: string;
+  description: string;
+  created: string;
+  price: string | number;
+  owner: string;
+  uid: string;
+  whatsapp: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps{
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export function CarDatail() {
+
+  const { id } = useParams();
+  const [car, setCar] = useState<CarsProps>();
+
+  useEffect(() => {
+
+    async function loadCar() {
+      if(!id) { return }
+
+      const docRef = doc(db, "cars", id);
+
+      getDoc(docRef)
+      .then((snapshot) => {
+        setCar({
+          id: snapshot.id,
+          name: snapshot.data()?.name,
+          year: snapshot.data()?.year,
+          city: snapshot.data()?.city,
+          model: snapshot.data()?.model,
+          uid: snapshot.data()?.uid,
+          description: snapshot.data()?.description,
+          created: snapshot.data()?.created,
+          whatsapp: snapshot.data()?.whatsapp,
+          price: snapshot.data()?.price,
+          km: snapshot.data()?.km,
+          owner: snapshot.data()?.owner,
+          images: snapshot.data()?.images
+        })
+      })
+    }
+
+    loadCar();
+
+  }, [id])
+
+
+
     return (
-        <div>
-          <h1>Pagina Detalhes</h1>
-        </div>
+      <Container>
+        <h1>SLIDER</h1>
+
+        { car && (
+          <main className="w-full bg-white rounded-lg p-6 my-4">
+            <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
+              <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
+              <h1 className="font-bold text-3xl text-black">R$ {car?.price}</h1>
+            </div>
+
+            <p>{car.model}</p>
+
+            <div className="flex w-full gap-6 my-4">
+              <div className="flex flex-col gap-4">
+                <div>
+                  <p>Cidade</p>
+                  <strong>{car?.city}</strong>
+                </div>
+                <div>
+                  <p>Ano</p>
+                  <strong>{car?.year}</strong>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-4">
+                <div>
+                  <p>KM</p>
+                  <strong>{car?.km}</strong>
+                </div>
+              </div>
+            </div>
+
+            <strong>Descrição:</strong>
+            <p className="mb-4">{car?.description}</p>
+
+            <strong>Telefone / Whastapp</strong>
+            <p>{car?.whatsapp}</p>
+
+            <a
+              className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-xl font-medium"
+            >
+              Conversar com o vendedor
+              <FaWhatsapp size={26} color="#FFF" />
+            </a>
+
+
+          </main>
+        )}
+      </Container>
     )
 }
